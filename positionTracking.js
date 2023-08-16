235ceedf3b75ac417a5700cdfef19b485bc60fe0
Rares <rares.s013@gmail.com>
2023-08-16 13:51:11 +0300

    Full functionality of range tracking for this extension

--- positionTracking.js
+++ positionTracking.js
@@ -10,20 +10,26 @@ const updatePositionDueToInsertion = (positionLine, positionCharacter, contentCh
 
          const index = contentChange.text.lastIndexOf('\n')
          positionCharacter += contentChange.text.slice(index + 1, contentChange.text.length).length
+
          // the insertion has no new lines
       } else {
          positionCharacter += contentChange.text.length
       }
    }
+
    positionLine += contentChange.text.split('\n').length - 1
 
    return [positionLine, positionCharacter]
 }
 
+/**
+ * @param {vscode.TextDocumentChangeEvent} event
+ * @param {vscode.Range[]} ranges
+ */
 const getUpdatedRanges = (event, ranges) => {
-   let updatedRanges = [...ranges]
+   const toUpdateRanges = [...ranges]
 
-   if (!updatedRanges) {
+   if (!toUpdateRanges) {
       return
    }
 
@@ -45,66 +51,80 @@ const getUpdatedRanges = (event, ranges) => {
       outputChannel.appendLine(`    -----`)
    }
    outputChannel.appendLine('Marker positions')
-   for (const markerRange of updatedRanges) {
+   for (const markerRange of toUpdateRanges) {
       outputChannel.appendLine(`    ${markerRange.start.line} ${markerRange.start.character}`)
       outputChannel.appendLine(`    -----`)
    }
    // debug logging - end
 
    for (const change of sortedChanges) {
-      for (let i = 0; i < updatedRanges.length; i++) {
-         if (!updatedRanges[i]) {
+      for (let i = 0; i < toUpdateRanges.length; i++) {
+         if (!toUpdateRanges[i]) {
             continue
          }
 
-         let newMarkerPositionLine = updatedRanges[i].start.line
-         let newMarkerPositionCharacter = updatedRanges[i].start.character
+         let newRangeStartLine = toUpdateRanges[i].start.line
+         let newRangeStartCharacter = toUpdateRanges[i].start.character
 
          // change before marker
-         if (change.range.end.isBefore(updatedRanges[i].start)) {
+         if (change.range.end.isBefore(toUpdateRanges[i].start)) {
             // change consisted in deleting
             if (!change.range.start.isEqual(change.range.end)) {
                // change range is also on the marker's line
-               if (change.range.end.line === newMarkerPositionLine) {
+               if (change.range.end.line === newRangeStartLine) {
                   const characterDelta = change.range.end.character - change.range.start.character
-                  newMarkerPositionCharacter -= characterDelta
+                  newRangeStartCharacter -= characterDelta
                }
                const lineDelta = change.range.end.line - change.range.start.line
-               newMarkerPositionLine -= lineDelta
+               newRangeStartLine -= lineDelta
 
                // change consisted also in insertion
                if (change.text) {
                   // eslint-disable-next-line no-extra-semi
-                  ;[newMarkerPositionLine, newMarkerPositionCharacter] =
-                     updatePositionDueToInsertion(
-                        newMarkerPositionLine,
-                        newMarkerPositionCharacter,
-                        change
-                     )
+                  ;[newRangeStartLine, newRangeStartCharacter] = updatePositionDueToInsertion(
+                     newRangeStartLine,
+                     newRangeStartCharacter,
+                     change
+                  )
                }
 
                // change consisted in insertion
             } else {
                // eslint-disable-next-line no-extra-semi
-               ;[newMarkerPositionLine, newMarkerPositionCharacter] = updatePositionDueToInsertion(
-                  newMarkerPositionLine,
-                  newMarkerPositionCharacter,
+               ;[newRangeStartLine, newRangeStartCharacter] = updatePositionDueToInsertion(
+                  newRangeStartLine,
+                  newRangeStartCharacter,
                   change
                )
             }
 
-            const position = new vscode.Position(newMarkerPositionLine, newMarkerPositionCharacter)
-            updatedRanges[i] = new vscode.Range(position, position)
-         } else if (
-            change.range.start.isBeforeOrEqual(updatedRanges[i].start) &&
-            change.range.end.isAfterOrEqual(updatedRanges[i].start)
-         ) {
-            updatedRanges[i] = null
+            const newRangeStart = new vscode.Position(newRangeStartLine, newRangeStartCharacter)
+
+            const lineDelta = toUpdateRanges[i].end.line - toUpdateRanges[i].start.line
+            const characterDelta =
+               toUpdateRanges[i].end.character - toUpdateRanges[i].start.character
+
+            let newRangeEnd = undefined
+            if (lineDelta !== 0) {
+               newRangeEnd = new vscode.Position(
+                  newRangeStart.line + lineDelta,
+                  toUpdateRanges[i].end.character
+               )
+            } else {
+               newRangeEnd = new vscode.Position(
+                  newRangeStart.line,
+                  newRangeStart.character + characterDelta
+               )
+            }
+
+            toUpdateRanges[i] = new vscode.Range(newRangeStart, newRangeEnd)
+         } else if (change.range.intersection(toUpdateRanges[i])) {
+            toUpdateRanges[i] = null
          }
       }
    }
 
-   updatedRanges = updatedRanges.filter((range) => range)
+   const updatedRanges = toUpdateRanges.filter((range) => range)
 
    return updatedRanges
 }
